6bb664071e9808500e7a7dc56accfc2d4c89f726
mugworks <greenwood.pdx@gmail.com>
2017-10-09 14:15:08 -0700

    set up done = prob not solved

---
+++ tests/arrayNum.test.js
@@ -0,0 +1,21 @@
+const arrayNum = require('../lib/arrayNum');
+const assert = require('assert');
+
+
+
+let arrayNum = [];
+
+describe('testing an array of numbers and filtering', () => {
+
+    beforeEach(function() {
+        arrayNum = new arrayNum;
+    })
+
+    it('should take an array of numbers, square them and filter to > 20', () => {
+        const mappedArray = getAllObj.map((obj) => {
+        return array;
+        assert.deepEqual(array, [25, 49]);
+    });
+
+
+});
