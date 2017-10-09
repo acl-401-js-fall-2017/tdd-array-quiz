87bfc9909e93b7e4e80420cb340db1871fcebd05
Robin Miller <rmillerpdx@gmail.com>
2017-10-09 13:57:14 -0700

    array method passes test

--- test/array-test.js
+++ test/array-test.js
@@ -5,6 +5,6 @@ describe('array method', () => {
 
     it('should return squares of array elements which are 20 or greater', () => {
         const testArray = [3,4,5,6];
-        assert.equal(arrayMethod(testArray), [25, 36] );
+        assert.deepEqual(arrayMethod(testArray), [25, 36] );
     });
 });
