5ae26f7112c1397390302cb92d4808dc13a97f2f
ckrountree <jmail0207@gmail.com>
2017-10-09 14:15:31 -0700

    create bones of quiz

---
+++ test/quiz.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+describe('take and array and return a new array with numbers that are squared and filtered') => {
+    const createsFilteredArr = (num) =>{
+        return item;
+    };
+
+    it('filter', () => {
+        const initArr = [];
+        const filteredArr = filter(initArr, createsFilteredArr);
+
+        aseert.deepEqual(filteredArr, initArr[]);
+    });
+}
