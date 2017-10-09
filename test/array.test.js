ddf30e33683d8ba9525217a74c6f7fff7b8bbcb5
Zac-L <Zachlichens@gmail.com>
2017-10-09 14:16:26 -0700

    created a test for testing squared numbers

---
+++ test/array.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const numbers = require('../lib/numbers');
+
+
+describe('takes an array of numbers', () => {
+    let array = [];
+
+    it('takes an array, returns array', done => {
+
+        numbers(array, (err, returnedArray) => {
+            if(err) return done(err);
+            assert.equal(returnedArray, 25, 49);
+            done();
+        });
+
+    });
+});
